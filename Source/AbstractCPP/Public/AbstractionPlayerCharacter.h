f1c3d83d5917a96ae03aa8546f4f7534ec7fa7b0
Lordnickim <nicholaskim101@yahoo.com>
2021-10-06 22:06:43 -0700

    Abstracted Player Controller and Player Character
    -Created two Classes to Abstract UE4s PlayerController and PlayerCharacter.
    -Replaced in BluePrints the CustomPlayerController and CustomPlayerCharacter parent class to the new Abstracted Classes.
    -Override FellOutOfWorld function with own.

---
+++ Source/AbstractCPP/Public/AbstractionPlayerCharacter.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbstractionPlayerCharacter.generated.h"
+
+UCLASS()
+class ABSTRACTCPP_API AAbstractionPlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AAbstractionPlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	/** Called when the actor falls out of the world 'safely' (below KillZ and such) */
+	virtual void FellOutOfWorld(const class UDamageType& dmgType) override;
+
+};
